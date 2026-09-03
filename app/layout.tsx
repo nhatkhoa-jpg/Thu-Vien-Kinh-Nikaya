735bb72169714bc9e81263f164f06f0fa60c566b
nhatkhoa-jpg <nhatkhoa@gmail.com>
2026-09-04 03:34:04 +0700

    Load sitewide Buddhist design system

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import './canon.css';
 import './stats.css';
 import './high-contrast-ui.css';
 import './buddhist-home.css';
+import './sitewide-buddhist.css';
 import type {Metadata} from 'next';
 import SiteStatsTracker from '@/components/SiteStatsTracker';
 
